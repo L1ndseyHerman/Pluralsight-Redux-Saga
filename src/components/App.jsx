63c54f13e9ad49fbca4565f822a71e4a02cfd867
Lindsey Herman <lherman@onshift.com>
2022-04-05 22:30:59 -0400

    Caught up to vid6 Module5, Take, Call, Put

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
-import { getStore } from '../getStore';
-import React from 'react';
-import { Provider } from 'react-redux'
-import { MainContainer } from '../components'
-import { getCurrentUserInfo } from '../actions'
+import { getStore } from "../getStore";
+import React from "react";
+import { Provider } from "react-redux";
+import { MainContainer } from "../components";
+import { getCurrentUserInfo } from "../actions";
 const store = getStore();
-export const App = ()=>(
-    <div>
-        <Provider store={store}>
-            <MainContainer/>
-        </Provider>
-    </div>
+export const App = () => (
+  <div>
+    <Provider store={store}>
+      <MainContainer />
+    </Provider>
+  </div>
 );
 
+//  This pretends to check the user from a cookie or something:
 store.dispatch(getCurrentUserInfo(`U10000`));
--- src/sagas/currentUserSaga.js
+++ src/sagas/currentUserSaga.js
@@ -1,11 +1,14 @@
-//  HIS IMPORT IS DEPRECATED, "DELAY" COMES FROM "REDUX-SAGA/EFFECTS NOW!!"
-import { delay } from "redux-saga/effects";
+//  Something abt apply changes the scope of call?
+import { take, put, call, apply } from "redux-saga/effects";
+//  Lets u call the API... but doesn't React come w a "fetch()"?
+import fetch from "isomorphic-fetch";
+
+import { GET_CURRENT_USER_INFO, setCurrentUser } from "./../actions";
+
 //  "function*" means that it's a generator function:
 export function* currentUserSaga() {
-  //  Something abt this would normally crash the computer, but doesn't w a delay?
-  while (true) {
-    //    Won't run until this much time has passed:
-    yield delay(1000);
-    console.info("User saga loop");
-  }
+  const { id } = yield take(GET_CURRENT_USER_INFO);
+  const response = yield call(fetch, `http://localhost:8081/user/${id}`);
+  const data = yield apply(response, response.json);
+  yield put(setCurrentUser(data));
 }
